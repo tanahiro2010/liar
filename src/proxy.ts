e3743fbd0796e03c73a7f0ecfb94e32f229b7cfb
tanahiro2010 <herentongkegu087@gmail.com>
2025-12-04 21:37:19 +0900

    fix: remove auth header section

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,9 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { authClient } from './lib/auth-client';
 
 export default async function proxy(request: NextRequest) {
     const requestHeaders = new Headers(request.headers);
     requestHeaders.set('x-pathname', request.nextUrl.pathname);
-    const session = await authClient.getSession();
-    requestHeaders.set('x-is-authenticated', session.data ? 'true' : 'false');
 
     return NextResponse.next({
         request: {
