f9eb1fb9c98eaacd24aca1249aa115409676e7aa
tanahiro2010 <herentongkegu087@gmail.com>
2025-12-04 17:45:04 +0900

    add: header component

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,60 @@
+import { headers } from "next/headers";
+import Link from "next/link";
+import "@/styles/header.css";
+import "@/styles/navigation.css";
+
+const navigationLinks = [
+    { name: "国内", slug: "domestic" },
+    { name: "国際", slug: "international" },
+    { name: "経済", slug: "economy" },
+    { name: "エンタメ", slug: "entertainment" },
+    { name: "スポーツ", slug: "sports" },
+    { name: "IT", slug: "it" },
+    { name: "科学", slug: "science" },
+    { name: "ライフ", slug: "life" },
+    { name: "地域", slug: "local" },
+];
+
+export default async function Header() {
+    const headersList = await headers();
+    const pathname = headersList.get("x-pathname") || "/";
+    const isAuthenticated = headersList.get("x-is-authenticated") === "true";
+
+    return (
+        <header>
+            <div className="header-top">
+                <div className="header-container">
+                    <a href="#" className="logo">Lair News!</a>
+                    <div className="search-box">
+                        <input type="text" placeholder="キーワードを入力" />
+                    </div>
+                    <div className="header-links">
+                        {isAuthenticated ? (
+                            <>
+                                <a href="/profile">マイページ</a>
+                                <a href="/auth/logout">ログアウト</a>
+                            </>
+                        ) : (
+                            <>
+                                <a href="/auth/login">ログイン</a>
+                                <a href="/auth/register">新規取得</a>
+                            </>
+                        )}
+
+                    </div>
+                </div>
+            </div>
+            <nav className="nav-bar">
+                <div className="nav-container">
+                    {/* Navigation Links */}
+                    <Link href={`/`} className={pathname === "/" ? "active" : ""}>トップ</Link>
+                    {navigationLinks.map((link: { name: string; slug: string }) => (
+                        <Link key={link.name} href={`/category/${link.slug}`} className={pathname === `/category/${link.slug}` ? "active" : ""}>
+                            {link.name}
+                        </Link>
+                    ))}
+                </div>
+            </nav>
+        </header>
+    )
+}
