73cb9e4cc9aec216a88f8a9f1f1099d89aa5d04a
tanahiro2010 <herentongkegu087@gmail.com>
2025-12-06 20:47:10 +0900

    add: auth

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,6 +1,15 @@
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 
 export default async function ProtectedLayout({ children }: { children: React.ReactNode }) {
-    const session = await auth.api.getSession();
+    const headersList = await headers();
+    const session = await auth.api.getSession({
+        headers: headersList,
+    });
+    if (!session?.user) {
+        redirect("/auth/login");
+    }
+
     return <>{children}</>;
 }
