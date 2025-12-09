e65d8f2b567dd8b6c659233db166410c478f59a4
tanahiro2010 <herentongkegu087@gmail.com>
2025-12-09 16:50:31 +0900

    add: some settings

--- src/app/api/og/news/route.tsx
+++ src/app/api/og/news/route.tsx
@@ -2,6 +2,10 @@
 import { ImageResponse } from "next/og";
 import { NewsOGP } from "@/components/ogp/news";
 
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+export const contentType = 'image/png';
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const title = searchParams.get("title") ?? "test";
--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -2,7 +2,11 @@
 import { ImageResponse } from "next/og";
 import { DefaultOGP } from "@/components/ogp/default";
 
-export async function GET(request: Request) {
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+export const contentType = 'image/png';
+
+export async function GET() {
   // デフォルトOGP（サイト全体用）
   return new ImageResponse(
     <DefaultOGP />
