919ebf59e5d9d368a540f6faee36d399dfa13353
tanahiro2010 <herentongkegu087@gmail.com>
2025-12-13 15:22:23 +0900

    fix: change time

--- src/components/utils/auto-reload.tsx
+++ src/components/utils/auto-reload.tsx
@@ -3,16 +3,19 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export function AutoReload() {
-    const router = useRouter();
+  const router = useRouter();
 
-    useEffect(() => {
-        // 5分ごとにリロード
-        const interval = setInterval(() => {
-            router.refresh();
-        }, 5 * 1000);
+  useEffect(() => {
+    // 5分ごとにリロード
+    const interval = setInterval(
+      () => {
+        router.refresh();
+      },
+      5 * 60 * 1000,
+    );
 
-        return () => clearInterval(interval);
-    }, [router]);
+    return () => clearInterval(interval);
+  }, [router]);
 
-    return null;
+  return null;
 }
